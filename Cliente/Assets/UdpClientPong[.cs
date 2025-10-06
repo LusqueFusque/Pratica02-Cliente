ffe8eda20a1c5cef15eb796a9e9cb5ab6cee9646
LusqueFusque <145374963+LusqueFusque@users.noreply.github.com>
2025-10-06 17:41:30 -0300

    1

--- Cliente/Assets/UdpClientPong[.cs
+++ Cliente/Assets/UdpClientPong[.cs
@@ -1,64 +1,81 @@
+using System.Collections.Concurrent; // para ConcurrentQueue
 using UnityEngine;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Globalization;
 
 public class UdpClientPong : MonoBehaviour
+{
+    UdpClient client;
+    Thread receiveThread;
+    IPEndPoint serverEP;
+
+    public GameObject localCube;
+    public GameObject remoteCube;
+    public GameObject ball;
+
+    int myId = -1;
+
+    // Variáveis thread-safe para armazenar posições recebidas
+    private ConcurrentQueue<Vector3> remotePositionsQueue = new ConcurrentQueue<Vector3>();
+    private ConcurrentQueue<Vector3> ballPositionsQueue = new ConcurrentQueue<Vector3>();
+
+    void Start()
     {
-        UdpClient client; 
-        Thread receiveThread;
-        IPEndPoint serverEP;
-        int myId = -1;
-        public GameObject localCube;
-        public GameObject remoteCube; // o jogador remoto
-        public GameObject remoteBall; // a bolinha
-        Vector3 remoteBallPos;
-        Vector3 remotePosition = Vector3.zero;
+        client = new UdpClient();
+        serverEP = new IPEndPoint(IPAddress.Parse("10.57.1.35"), 5001);
+        client.Connect(serverEP);
 
-        void Start()
+        receiveThread = new Thread(ReceiveData);
+        receiveThread.IsBackground = true; // garante que thread será finalizada quando fechar app
+        receiveThread.Start();
+
+        byte[] hello = Encoding.UTF8.GetBytes("HELLO");
+        client.Send(hello, hello.Length);
+    }
+
+    void Update()
+    {
+        // Movimento do cubo local
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        localCube.transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * 5);
+
+        // Envia posição para o servidor
+        string msg = "POS:" + localCube.transform.position.x.ToString("F2", CultureInfo.InvariantCulture) + ";" + localCube.transform.position.y.ToString("F2", CultureInfo.InvariantCulture);
+        byte[] data = Encoding.UTF8.GetBytes(msg);
+        client.Send(data, data.Length);
+
+        // Aplica posição recebida para remoteCube (se houver)
+        if (remotePositionsQueue.TryDequeue(out Vector3 remotePos))
         {
-            client = new UdpClient(); 
-            serverEP = new IPEndPoint(IPAddress.Parse("10.57.1.35"), 5001);
-            client.Connect(serverEP); 
-            
-            // Thread para ouvir respostas do servidor
-            receiveThread = new Thread(ReceiveData);
-            receiveThread.Start();
-
-            // Envia mensagem inicial para o servidor
-            byte[] hello = Encoding.UTF8.GetBytes("HELLO");
-            client.Send(hello, hello.Length); 
+            remoteCube.transform.position = Vector3.Lerp(remoteCube.transform.position, remotePos, Time.deltaTime * 10f);
         }
-        void Update()
-        { 
-            // Movimenta o cubo local
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-            localCube.transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * 5); 
-            
-            // Envia posição formatada
-            string msg = "POS:" + localCube.transform.position.x.ToString("F2", CultureInfo.InvariantCulture) + ";" + localCube.transform.position.y.ToString("F2", CultureInfo.InvariantCulture);
-            byte[] data = Encoding.UTF8.GetBytes(msg);
-            client.Send(data, data.Length);
-            
-            remoteCube.transform.position = Vector3.Lerp(remoteCube.transform.position, remotePosition, Time.deltaTime * 10f);
-            remoteBall.transform.position = Vector3.Lerp(remoteBall.transform.position, remoteBallPos, Time.deltaTime * 10f);
 
-        }
-        void ReceiveData()
+        // Aplica posição recebida para bola (se houver)
+        if (ballPositionsQueue.TryDequeue(out Vector3 ballPos))
         {
-            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0); 
-            while (true)
+            ball.transform.position = Vector3.Lerp(ball.transform.position, ballPos, Time.deltaTime * 10f);
+        }
+    }
+
+    void ReceiveData()
+    {
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+        while (true)
+        {
+            try
             {
                 byte[] data = client.Receive(ref remoteEP);
                 string msg = Encoding.UTF8.GetString(data);
-        
+
                 if (msg.StartsWith("ASSIGN:"))
                 {
                     myId = int.Parse(msg.Substring(7));
-                    Debug.Log("Meu ID: " + myId);
+                    Debug.Log("[Cliente] Recebi ID = " + myId);
                 }
                 else if (msg.StartsWith("PLAYER:"))
                 {
@@ -69,21 +86,33 @@ public class UdpClientPong : MonoBehaviour
                     string[] coords = parts[2].Split(';');
                     float x = float.Parse(coords[0], CultureInfo.InvariantCulture);
                     float y = float.Parse(coords[1], CultureInfo.InvariantCulture);
-                    remoteCube.transform.position = new Vector3(x, y, 0);
+
+                    remotePositionsQueue.Enqueue(new Vector3(x, y, 0));
                 }
                 else if (msg.StartsWith("BALL:"))
                 {
                     string[] coords = msg.Substring(5).Split(';');
                     float x = float.Parse(coords[0], CultureInfo.InvariantCulture);
                     float y = float.Parse(coords[1], CultureInfo.InvariantCulture);
-                    remoteBallPos = new Vector3(x, y, 0);
+
+                    ballPositionsQueue.Enqueue(new Vector3(x, y, 0));
                 }
             }
-        
+            catch (SocketException ex)
+            {
+                Debug.LogWarning("Socket encerrado ou erro: " + ex.Message);
+                break;
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Erro no cliente: " + ex.Message);
+            }
         }
-        void OnApplicationQuit()
-        {
-            receiveThread.Abort(); 
-            client.Close(); 
-        } 
     }
+
+    void OnApplicationQuit()
+    {
+        receiveThread?.Abort();
+        client?.Close();
+    }
+}
